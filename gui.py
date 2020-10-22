636f66f3f7632288319701076a55f146195d267c
reborafs <francor.96@gmail.com>
2020-10-21 21:37:32 -0300

    add button

--- gui.py
+++ gui.py
@@ -5,46 +5,71 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
                                                NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+
+class Application(tk.Frame):
+    pass
 
 # Open tkinter class.
 root=tk.Tk()
 
-# Main window features.
+# Root config.
 root.title("Wave Generator")
-root.config(bg="black")
+root.config(bg="white")
 
-# Create Frame
-frame = tk.Frame(root, width=650, height=350, bg='red')
-frame.pack()
+# Creating Frames
+graph_frame = tk.Frame(root, width=1200, height=600)
+graph_frame.pack(fill='both', expand='True')
+graph_frame.config(bd=35)
 
-# Wave generator.
-tk.Label(frame, text='Wave Generator.').place(x=100, y=200)
+# Functions.
+    
+def plotting():
+    pass
 
-# tk.Label(root, text="Frequency [Hz]").grid(row=0)
-# tk.Label(root, text="Time [secs]").grid(row=1)
-# freq = tk.Entry(root)
-# time = tk.Entry(root)
+def submit():
+    freq = float(entryFreq.get())
+    time = float(entryTime.get())
+    wave_type = 'sawtooth'
+    wave = Wave(freq, time, wave_type)
+    # fig.add_subplot(111).plot(wave.timevector, wave.wave)
+    plt.plot(wave.timevector, wave.wave)
 
-# # Grid placing.
-# freq.grid(row=0, column=1)
-# time.grid(row=1, column=1)
+# Creating Labels and Entries.
+labelTitle = tk.Label(graph_frame, text='Insert Data.')
+labelFreq = tk.Label(graph_frame, text='Frequency [Hz]')
+labelTime = tk.Label(graph_frame, text='Time [secs]')
+freq = tk.IntVar()
+time = tk.IntVar()
+entryFreq = tk.Entry(graph_frame)
+entryTime = tk.Entry(graph_frame)
+submitButton = tk.Button(graph_frame, text='Submit', command=submit)
+   
+    
+# Grid placing.
+labelTitle.grid(row=0)
+labelFreq.grid(row=1, column=0, sticky='e', padx=5, pady=5)
+labelTime.grid(row=2, column=0, sticky='e', padx=5, pady=5)
+entryFreq.grid(row=1, column=1)
+entryTime.grid(row=2, column=1)
+submitButton.grid(row=3, column=1)
 
-# wave_type = 'sawtooth'
-# wave = Wave(freq, time, wave_type)
+# Creates Figure.
+fig = Figure(figsize=(5, 4), dpi=100)
+time_vector = np.linspace(0,1,44100)
+wave = np.sin(2*np.pi*time_vector)
+fig.add_subplot(111).plot(time_vector, wave)
 
-# # Creates Figure.
-# fig = Figure(figsize=(5, 4), dpi=100)
-# fig.add_subplot(111).plot(wave.timevector, wave.wave)
+# A tk.DrawingArea.
+canvas = FigureCanvasTkAgg(fig, master=root)  
+canvas.draw()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
-# # A tk.DrawingArea.
-# canvas = FigureCanvasTkAgg(fig, master=root)  
-# canvas.draw()
-# canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
-# # Matplotlib Toolbar
-# toolbar = NavigationToolbar2Tk(canvas, root)
-# toolbar.update()
-# canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+# Matplotlib Toolbar
+toolbar = NavigationToolbar2Tk(canvas, root)
+toolbar.update()
+canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 # Closing the loop.
 root.mainloop() 
