faf4cc5524f32a8a68f3933903b80fb305df37c7
reborafs <francor.96@gmail.com>
2020-10-23 19:52:41 -0300

    can refresh plots

--- gui.py
+++ gui.py
@@ -6,16 +6,23 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+import sys
 
 class PlottingFrame(tk.Frame):
     def __init__(self, root):
         self.root = root
+        self.plotButton=tk.Button(master=root, text="Plot", command=lambda: self.plot(canvas,ax))
+        self.plotButton.pack(side=tk.TOP)
+
         
         # Creates Figure.
-        fig = Figure(figsize=(5, 4), dpi=100)
         time_vector = np.linspace(0,1,44100)
         wave = np.sin(2*np.pi*time_vector)
-        fig.add_subplot(111).plot(time_vector, wave) 
+        
+        fig = plt.figure(figsize=(8,8))
+        ax = fig.add_subplot(1,1,1)
+        ax.plot(time_vector, wave)
+        
         # A tk.DrawingArea.
         canvas = FigureCanvasTkAgg(fig, master=root)  
         canvas.draw()
@@ -24,6 +31,14 @@ class PlottingFrame(tk.Frame):
         toolbar = NavigationToolbar2Tk(canvas, root)
         toolbar.update()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def plot(self,canvas,ax):
+        time_vector = np.linspace(0,1,44100)
+        wave = np.sin(2*np.pi*time_vector)
+        ax.plot(time_vector,wave,color='red')
+        canvas.draw()
+        ax.clear()
+
     
 class InputFrame:
     def __init__(self, root):
@@ -35,8 +50,8 @@ class InputFrame:
         tk.Label(self.inputFrame, text='Insert Data.').grid(row=0)
         tk.Label(self.inputFrame, text='Frequency [Hz]').grid(row=1, column=0, sticky='e', padx=5, pady=5)
         tk.Label(self.inputFrame, text='Time [secs]').grid(row=2, column=0, sticky='e', padx=5, pady=5)
-        self.submitButton = tk.Button(self.inputFrame, text='Play', command=self.submit)
-
+        self.playButton = tk.Button(self.inputFrame, text='Play', command=self.play)
+        
         
         self.freq = tk.IntVar()
         self.time = tk.IntVar()
@@ -44,9 +59,18 @@ class InputFrame:
         self.entryTime = tk.Entry(self.inputFrame)
         self.entryFreq.grid(row=1, column=1)
         self.entryTime.grid(row=2, column=1)    
-        self.submitButton.grid(row=3, column=1)
+        self.playButton.grid(row=3, column=0)
+        
 
-    def submit(self):
+    def plot(self,canvas,ax):
+        for line in sys.stdout: #infinite loop, reads data of a subprocess
+            theta=line[1]
+            r=line[2]
+            ax.plot(theta,r,linestyle="None",maker='o')
+            canvas.draw()
+            ax.clear()
+
+    def play(self):
         self.freq = float(self.entryFreq.get())
         self.time = float(self.entryTime.get())
         self.wave_type = 'sine'
@@ -62,10 +86,11 @@ class Application:
         self.root.title("Wave Generator")
         self.root.config(bg="white")
         self.root.geometry('800x600')
-
+        
         tk.Label(self.root, text='Wave Generator').pack(side="top")
         self.inputFrame = InputFrame(self.root) 
         self.plottingFrame = PlottingFrame(self.root)
+
 
 
 
